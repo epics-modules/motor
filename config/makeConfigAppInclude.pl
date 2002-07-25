61faef5f93357ba2e0a82d16bea4ccdf6f5eec81
Ron Sluiter <rsluiter@users.noreply.github.com>
2002-07-25 20:45:42 +0000

    Bug fix for spaces between macro and "=" sign; e.g. MPF = /home/mpf.

--- config/makeConfigAppInclude.pl
+++ config/makeConfigAppInclude.pl
@@ -70,6 +70,8 @@ foreach $file (@files) {
 		    $post = $base . $post;
 		}
 	    }
+	    
+	    $prefix =~ s/^\s+|\s+$//g; # strip leading and trailing whitespace.
 	    $applications{$prefix} = $post;
 	    if ( -d "$post") { #check that directory exists
 		print OUT "\n";
--- config/makeIocCdCommands.pl
+++ config/makeIocCdCommands.pl
@@ -72,6 +72,8 @@ foreach $file (@files) {
 			$post = $base . $post;
 		    }
 		}
+		
+		$prefix =~ s/^\s+|\s+$//g; # strip leading and trailing whitespace.
 		$applications{$prefix} = $post;
 		$app = lc($prefix);
 		if ( -d "$post") { #check that directory exists
