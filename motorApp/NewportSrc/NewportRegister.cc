67dd775f325802e7e1edf8ab8ce019daa98f46e5
Ron Sluiter <rsluiter@users.noreply.github.com>
2004-07-16 19:28:01 +0000

    Removed unused <driver>Setup() argument.

--- motorApp/NewportSrc/NewportRegister.cc
+++ motorApp/NewportSrc/NewportRegister.cc
@@ -2,9 +2,9 @@
 FILENAME...	NewportRegister.cc
 USAGE...	Register Newport motor device driver shell commands.
 
-Version:	$Revision: 1.3 $
-Modified By:	$Author: rivers $
-Last Modified:	$Date: 2004-04-20 20:57:23 $
+Version:	$Revision: 1.4 $
+Modified By:	$Author: sluiter $
+Last Modified:	$Date: 2004-07-16 19:28:01 $
 */
 
 /*****************************************************************
@@ -27,23 +27,21 @@ extern "C"
 
 // Newport Setup arguments
 static const iocshArg setupArg0 = {"Max. controller count", iocshArgInt};
-static const iocshArg setupArg1 = {"N/A", iocshArgInt};
-static const iocshArg setupArg2 = {"Polling rate", iocshArgInt};
+static const iocshArg setupArg1 = {"Polling rate", iocshArgInt};
 // Newport Config arguments
 static const iocshArg configArg0 = {"Card being configured", iocshArgInt};
 static const iocshArg configArg1 = {"asyn port name", iocshArgString};
 static const iocshArg configArg2 = {"asyn address (GPIB)", iocshArgInt};
 
 
-static const iocshArg * const NewportSetupArgs[3] = {&setupArg0, &setupArg1,
-    &setupArg2};
+static const iocshArg * const NewportSetupArgs[2] = {&setupArg0, &setupArg1};
 static const iocshArg * const NewportConfigArgs[3] = {&configArg0, &configArg1,
     &configArg2};
 
-static const iocshFuncDef setupMM3000 = {"MM300Setup", 3, NewportSetupArgs};
-static const iocshFuncDef setupMM4000 = {"MM4000Setup",3, NewportSetupArgs};
-static const iocshFuncDef setupPM500  = {"PM500Setup", 3, NewportSetupArgs};
-static const iocshFuncDef setupESP300 = {"ESP300Setup",3, NewportSetupArgs};
+static const iocshFuncDef setupMM3000 = {"MM300Setup", 2, NewportSetupArgs};
+static const iocshFuncDef setupMM4000 = {"MM4000Setup",2, NewportSetupArgs};
+static const iocshFuncDef setupPM500  = {"PM500Setup", 2, NewportSetupArgs};
+static const iocshFuncDef setupESP300 = {"ESP300Setup",2, NewportSetupArgs};
 
 static const iocshFuncDef configMM3000 = {"MM3000Config", 3, NewportConfigArgs};
 static const iocshFuncDef configMM4000 = {"MM4000Config", 3, NewportConfigArgs};
@@ -52,35 +50,39 @@ static const iocshFuncDef configESP300 = {"ESP300Config", 3, NewportConfigArgs};
 
 static void setupMM3000CallFunc(const iocshArgBuf *args)
 {
-    MM3000Setup(args[0].ival, args[1].ival, args[2].ival);
+    MM3000Setup(args[0].ival, args[1].ival);
 }
 
 static void setupMM4000CallFunc(const iocshArgBuf *args)
 {
-    MM4000Setup(args[0].ival, args[1].ival, args[2].ival);
+    MM4000Setup(args[0].ival, args[1].ival);
 }
 
 static void setupPM500CallFunc(const iocshArgBuf *args)
 {
-    PM500Setup(args[0].ival, args[1].ival, args[2].ival);
+    PM500Setup(args[0].ival, args[1].ival);
 }
+
 static void setupESP300CallFunc(const iocshArgBuf *args)
 {
-    ESP300Setup(args[0].ival, args[1].ival, args[2].ival);
+    ESP300Setup(args[0].ival, args[1].ival);
 }
 
 static void configMM3000CallFunc(const iocshArgBuf *args)
 {
     MM3000Config(args[0].ival, args[1].sval, args[2].ival);
 }
+
 static void configMM4000CallFunc(const iocshArgBuf *args)
 {
     MM4000Config(args[0].ival, args[1].sval, args[2].ival);
 }
+
 static void configPM500CallFunc(const iocshArgBuf *args)
 {
     PM500Config(args[0].ival,  args[1].sval, args[2].ival);
 }
+
 static void configESP300CallFunc(const iocshArgBuf *args)
 {
     ESP300Config(args[0].ival, args[1].sval, args[2].ival);
