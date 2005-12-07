b147b8029341d3146790d72ee13b5a702acda16d
MarkRivers <rivers@cars.uchicago.edu>
2005-12-07 23:58:53 +0000

    Defines function prototype

---
+++ motorApp/NewportSrc/tclCall.h
@@ -0,0 +1,10 @@
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+epicsShareFunc void tclcall(char const *name,char const *taskName,char const *args);
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
