9f5b094bc1a90b62f88fdb1f53c8785a236f5377
MarkRivers <rivers@cars.uchicago.edu>
2006-03-30 03:00:22 +0000

    Unmodified file from 1.5.1 distribution

--- motorApp/NewportSrc/Socket.h
+++ motorApp/NewportSrc/Socket.h
@@ -1,9 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+// Socket.h
 
-int    ConnectToServer (char *Ip_Address, int Ip_Port, double Timeout);
-void   SetTCPTimeout(int SocketIndex, double TimeOut);
-void   SendAndReceive (int SocketIndex, char *buffer, char *valueRtrn);
-void   SendOnly (int SocketIndex, char *buffer, char *valueRtrn);
-void   CloseSocket (int SocketIndex);
-void   CloseAllSockets (void);
-void   ResetAllSockets (void);
-char * GetError (int SocketIndex);
+#include <afxsock.h>		// MFC socket extensions
+
+int  ConnectToServer (char *Ip_Address, int Ip_Port, double TimeOut);
+void SetTCPTimeout (int SocketID, double Timeout);
+void SendAndReceive(int socketID, char sSendString[], char sReturnString[], int iReturnStringSize);
+void CloseSocket (int SocketID);
+char * GetError (int SocketID);
+void strncpyWithEOS(char * szStringOut, const char * szStringIn, int nNumberOfCharToCopy, int nStringOutSize);
