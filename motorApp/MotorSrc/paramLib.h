5ac2736979225353baf3723519e18376a3472118
MarkRivers <rivers@cars.uchicago.edu>
2006-04-06 20:18:32 +0000

    Moved from MotorSimSrc

---
+++ motorApp/MotorSrc/paramLib.h
@@ -0,0 +1,33 @@
+#ifndef PARAM_LIB_H
+#define PARAM_LIB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PARAM_OK (0)
+#define PARAM_ERROR (-1)
+
+typedef unsigned int paramIndex;
+typedef struct paramList * PARAMS;
+typedef void (*paramCallback)( void *, unsigned int, unsigned int * ); 
+
+typedef struct
+{
+  PARAMS (*create)    ( paramIndex startVal, paramIndex nvals );
+  void (*destroy)     ( PARAMS params );
+  int  (*setInteger)  ( PARAMS params, paramIndex index, int value );
+  int  (*setDouble)   ( PARAMS params, paramIndex index, double value );
+  void (*callCallback)( PARAMS params );
+  int  (*getInteger)  ( PARAMS params, paramIndex index, int * value );
+  int  (*getDouble)   ( PARAMS params, paramIndex index, double * value );
+  int  (*setCallback) ( PARAMS params, paramCallback callback, void * param );
+  void (*dump)        ( PARAMS params );
+} paramSupport;
+
+extern paramSupport * motorParam;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
