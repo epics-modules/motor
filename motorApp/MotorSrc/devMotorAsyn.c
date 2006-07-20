296552768157ac773d1710e00783df8aa7ce40f8
Peter Denison <peter.denison@diamond.ac.uk>
2006-07-20 17:10:37 +0000

    Implement locking to prevent race condition where periodic status can arrive before a sent move command has reached the controller

--- motorApp/MotorSrc/devMotorAsyn.c
+++ motorApp/MotorSrc/devMotorAsyn.c
@@ -11,9 +11,9 @@
  * Notwithstanding the above, explicit permission is granted for APS to 
  * redistribute this software.
  *
- * Version: $Revision: 1.11 $
+ * Version: $Revision: 1.12 $
  * Modified by: $Author: peterd $
- * Last Modified: $Date: 2006-07-01 20:08:56 $
+ * Last Modified: $Date: 2006-07-20 17:10:37 $
  *
  * Original Author: Peter Denison
  * Current Author: Peter Denison
@@ -46,6 +46,7 @@
 #include "motor_interface.h"
 
 /*Create the dset for devMotor */
+static long init( int after );
 static long init_record(struct motorRecord *);
 static CALLBACK_VALUE update_values(struct motorRecord *);
 static long start_trans(struct motorRecord *);
@@ -58,7 +59,7 @@ typedef enum {int32Type, float64Type, float64ArrayType} interfaceType;
 
 struct motor_dset devMotorAsyn={ { 8,
 				NULL,
-				NULL,
+				(DEVSUPFUN) init,
 				(DEVSUPFUN) init_record,
                                   NULL },
 				update_values,
@@ -78,6 +79,7 @@ typedef struct {
 typedef struct
 {
     struct motorRecord * pmr;
+    int moveRequestPending;
     epicsUInt32 status;  /**< bit mask of errors and other binary information. The
 			 bit positions are in motor.h */
     epicsInt32 position; /**< Current motor position in motor steps (if not
@@ -100,6 +102,14 @@ typedef struct
 } motorAsynPvt;
 
 
+
+/* The init routine is used to set a flag to indicate that it is OK to call dbScanLock */
+static int dbScanLockOK = 0;
+static long init( int after )
+{
+    dbScanLockOK = (after!=0);
+    return 0;
+}
 
 static long init_record(struct motorRecord * pmr )
 {
@@ -326,6 +336,7 @@ static RTN_STATUS build_trans( motor_cmnd command,
 	pmsg->command = pPvt->move_cmd;
 	pmsg->dvalue = pPvt->param;
 	pPvt->move_cmd = -1;
+	pPvt->moveRequestPending++;
 	/* Do we need to set needUpdate and schedule a process here? */
 	/* or can we always guarantee to get at least one callback? */
 	/* Do we really need the callback? I assume so */
@@ -425,6 +436,7 @@ static void asynCallback(asynUser *pasynUser)
     motorRecord *pmr = pPvt->pmr;
     motorAsynMessage *pmsg = pasynUser->userData;
     int status;
+    int commandIsMove = 0;
 
     asynPrint(pasynUser, ASYN_TRACE_FLOW,
               "devMotorAsyn::asynCallback: %s command=%d, ivalue=%d, dvalue=%f\n",
@@ -442,9 +454,13 @@ static void asynCallback(asynUser *pasynUser)
 	pasynUser->reason = motorEncoderPosition;
 	pPvt->pasynInt32->read(pPvt->asynInt32Pvt, pasynUser,
 			       &pPvt->encoder_position);
-	scanOnce(pmr);
 	break;
 
+    case motorMoveAbs:
+    case motorMoveRel:
+    case motorHome:
+	commandIsMove = 1;
+	/* Intentional fall-through */
     default:
         if (pmsg->interface == int32Type) {
             pPvt->pasynInt32->write(pPvt->asynInt32Pvt, pasynUser,
@@ -455,6 +471,18 @@ static void asynCallback(asynUser *pasynUser)
         }
         break;
     }
+
+    if (dbScanLockOK) { /* effectively if iocInit has completed */
+	dbScanLock((dbCommon *)pmr);
+	if (commandIsMove) {
+	    pPvt->moveRequestPending--;
+	    if (!pPvt->moveRequestPending) {
+		pmr->rset->process((dbCommon*)pmr);
+	    }
+	}
+	dbScanUnlock((dbCommon *)pmr);
+    }
+
     pasynManager->memFree(pmsg, sizeof(*pmsg));
     status = pasynManager->freeAsynUser(pasynUser);
     if (status != asynSuccess) {
@@ -477,17 +505,17 @@ static void statusCallback(void *drvPvt, asynUser *pasynUser,
         "%s devMotorAsyn::statusCallback new value=0x%x\n",
         pmr->name, value);
 
-    if (interruptAccept) {
+    if (dbScanLockOK) {
         dbScanLock((dbCommon *)pmr);
         pPvt->status = value->status;
         pPvt->position = (epicsInt32)floor(value->position+0.5);
         pPvt->encoder_position = (epicsInt32)floor(value->encoder_posn+0.5);
-	/*	pPvt->veolcity = (epicsInt32)floor(value->velocity+0.5);*/
-        dbScanUnlock((dbCommon*)pmr);
-        if (!pPvt->needUpdate) {
+	/*	pPvt->velocity = (epicsInt32)floor(value->velocity+0.5);*/
+        if (!pPvt->needUpdate && !pPvt->moveRequestPending) {
 	    pPvt->needUpdate = 1;
-	    scanOnce(pmr);
+	    pmr->rset->process((dbCommon*)pmr);
         }
+        dbScanUnlock((dbCommon*)pmr);
     } else {
         pPvt->status = value->status;
         pPvt->position = (epicsInt32)floor(value->position+0.5);
