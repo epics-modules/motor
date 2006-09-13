17f97a0ccefe1b955c4e3dd6d5ef69e84a6d74be
Ron Sluiter <rsluiter@users.noreply.github.com>
2006-09-13 16:36:05 +0000

    Orginial

---
+++ motorApp/PiSrc/devPIC862.cc
@@ -0,0 +1,285 @@
+/*
+FILENAME...	devPIC862.cc
+USAGE...	Motor record device level support for Physik Instrumente (PI)
+		GmbH & Co. C-862 motor controller.
+*/
+
+/*
+ *      Original Author: Ron Sluiter
+ *      Current Author: Mohan Ramanathan
+ *      Date: 09/04/2006
+ *
+ * Modification Log:
+ * -----------------
+ * .00  09/05/2006  mr  copied from devPIC848.cc
+ */
+
+
+#include <string.h>
+#include "motorRecord.h"
+#include "motor.h"
+#include "motordevCom.h"
+#include "drvPIC862.h"
+#include "epicsExport.h"
+
+extern struct driver_table PIC862_access;
+
+/* ----------------Create the dsets for devPIC862----------------- */
+static struct driver_table *drvtabptr;
+static long PIC862_init(void *);
+static long PIC862_init_record(void *);
+static long PIC862_start_trans(struct motorRecord *);
+static RTN_STATUS PIC862_build_trans(motor_cmnd, double *, struct motorRecord *);
+static RTN_STATUS PIC862_end_trans(struct motorRecord *);
+
+struct motor_dset devPIC862 =
+{
+    {8, NULL, (DEVSUPFUN) PIC862_init, (DEVSUPFUN) PIC862_init_record, NULL},
+    motor_update_values,
+    PIC862_start_trans,
+    PIC862_build_trans,
+    PIC862_end_trans
+};
+
+extern "C" {epicsExportAddress(dset,devPIC862);}
+
+/* --------------------------- program data --------------------- */
+
+/* This table is used to define the command types */
+/* WARNING! this must match "motor_cmnd" in motor.h */
+
+static msg_types PIC862_table[] = {
+    MOTION, 	/* MOVE_ABS */
+    MOTION, 	/* MOVE_REL */
+    MOTION, 	/* HOME_FOR */
+    MOTION, 	/* HOME_REV */
+    IMMEDIATE,	/* LOAD_POS */
+    IMMEDIATE,	/* SET_VEL_BASE */
+    IMMEDIATE,	/* SET_VELOCITY */
+    IMMEDIATE,	/* SET_ACCEL */
+    IMMEDIATE,	/* GO */
+    IMMEDIATE,	/* SET_ENC_RATIO */
+    INFO,	/* GET_INFO */
+    MOVE_TERM,	/* STOP_AXIS */
+    VELOCITY,	/* JOG */
+    IMMEDIATE,	/* SET_PGAIN */
+    IMMEDIATE,	/* SET_IGAIN */
+    IMMEDIATE,	/* SET_DGAIN */
+    IMMEDIATE,	/* ENABLE_TORQUE */
+    IMMEDIATE,	/* DISABL_TORQUE */
+    IMMEDIATE,	/* PRIMITIVE */
+    IMMEDIATE,	/* SET_HIGH_LIMIT */
+    IMMEDIATE,	/* SET_LOW_LIMIT */
+    VELOCITY	/* JOG_VELOCITY */
+};
+
+
+static struct board_stat **PIC862_cards;
+
+/* --------------------------- program data --------------------- */
+
+
+/* initialize device support for PIC862 servo motor */
+static long PIC862_init(void *arg)
+{
+    long rtnval;
+    int after = (int) arg;
+
+    if (after == 0)
+    {
+	drvtabptr = &PIC862_access;
+	(drvtabptr->init)();
+    }
+
+    rtnval = motor_init_com(after, *drvtabptr->cardcnt_ptr, drvtabptr, &PIC862_cards);
+    return(rtnval);
+}
+
+
+/* initialize a record instance */
+static long PIC862_init_record(void *arg)
+{
+    struct motorRecord *mr = (struct motorRecord *) arg;
+    return(motor_init_record_com(mr, *drvtabptr->cardcnt_ptr, drvtabptr, PIC862_cards));
+}
+
+
+/* start building a transaction */
+static long PIC862_start_trans(struct motorRecord *mr)
+{
+    motor_start_trans_com(mr, PIC862_cards);
+    return(OK);
+}
+
+
+/* end building a transaction */
+static RTN_STATUS PIC862_end_trans(struct motorRecord *mr)
+{
+    motor_end_trans_com(mr, drvtabptr);
+    return(OK);
+}
+
+
+/* add a part to the transaction */
+static RTN_STATUS PIC862_build_trans(motor_cmnd command, double *parms, struct motorRecord *mr)
+{
+    struct motor_trans *trans = (struct motor_trans *) mr->dpvt;
+    struct mess_node *motor_call;
+    struct controller *brdptr;
+    char buff[110];
+    int card;
+    unsigned int size;
+    double dval;
+    int cntrl_units;
+    RTN_STATUS rtnval;
+    bool send;
+
+    send = true;		/* Default to send motor command. */
+    rtnval = OK;
+    buff[0] = '\0';
+    
+    /* Protect against NULL pointer with WRTITE_MSG(GO/STOP_AXIS/GET_INFO, NULL). */
+    dval = (parms == NULL) ? 0.0 : *parms;
+    
+
+    motor_call = &(trans->motor_call);
+    card = motor_call->card;
+    brdptr = (*trans->tabptr->card_array)[card];
+    if (brdptr == NULL)
+	return(rtnval = ERROR);
+
+
+    cntrl_units = (int) dval;
+    
+    if (PIC862_table[command] > motor_call->type)
+	motor_call->type = PIC862_table[command];
+
+    if (trans->state != BUILD_STATE)
+	return(rtnval = ERROR);
+
+    if (command == PRIMITIVE && mr->init != NULL && strlen(mr->init) != 0)
+	strcat(motor_call->message, mr->init);
+
+    switch (command)
+    {
+	case MOVE_ABS:
+	case MOVE_REL:
+	case HOME_FOR:
+	case HOME_REV:
+	case JOG:
+	    if (strlen(mr->prem) != 0)
+	    {
+		strcat(motor_call->message, mr->prem);
+		strcat(motor_call->message, ",");
+	    }
+	    if (strlen(mr->post) != 0)
+		motor_call->postmsgptr = (char *) &mr->post;
+	    break;
+        
+	default:
+	    break;
+    }
+
+
+    switch (command)
+    {
+	case MOVE_ABS:
+	    sprintf(buff, "MA%d,", cntrl_units);
+	    break;
+	
+	case MOVE_REL:
+	    sprintf(buff, "MR%d,", cntrl_units);
+	    break;
+	
+	case HOME_FOR:
+	    sprintf(buff, "FE0");
+	    break;
+	case HOME_REV:
+	    sprintf(buff, "FE1");
+	    break;
+	
+	case LOAD_POS:
+	    if (cntrl_units == 0.0)
+		sprintf(buff, "DH");
+	    else
+		rtnval = ERROR;
+	    break;
+	
+	case SET_VEL_BASE:
+	    send = false;	/* DC motor; not base velocity. */
+	    break;
+	
+        case JOG_VELOCITY:
+	case SET_VELOCITY:
+	    sprintf(buff, "SV%d,", cntrl_units);
+	    break;
+	
+	case SET_ACCEL:
+	    sprintf(buff, "SA%d,", cntrl_units);
+	    break;
+
+	case ENABLE_TORQUE:
+	    sprintf(buff, "MN");
+	    break;
+
+	case DISABL_TORQUE:
+	    sprintf(buff, "MF");
+	    break;
+
+	case GO:
+	    /* The PIC862 starts moving immediately on move commands, GO command
+	     * does nothing. */
+	    send = false;
+	    break;
+	
+	case PRIMITIVE:
+	case GET_INFO:
+	    /* These commands are not actually done by sending a message, but
+	       rather they will indirectly cause the driver to read the status
+	       of all motors */
+	    break;
+	
+	case STOP_AXIS:
+	    sprintf(buff, "ST");
+	    break;
+	
+	case JOG:
+	    sprintf(buff, "MR%d,", cntrl_units);
+	    break;
+	
+	case SET_PGAIN:
+	    cntrl_units = (int) (32767 * dval);
+	    sprintf(buff, "DP%d", cntrl_units);
+	    break;
+	case SET_IGAIN:
+	    cntrl_units = (int) (32767 * dval);
+	    sprintf(buff, "DI%d", cntrl_units);
+	    break;
+	case SET_DGAIN:
+	    cntrl_units = (int) (32767 * dval);
+	    sprintf(buff, "DD%d", cntrl_units);
+	    break;
+	
+	case SET_HIGH_LIMIT:
+	case SET_LOW_LIMIT:
+	case SET_ENC_RATIO:
+	    trans->state = IDLE_STATE;	/* No command sent to the controller. */
+	    send = false;
+	    break;
+	
+	default:
+	    send = false;
+	    rtnval = ERROR;
+    }
+
+    size = strlen(buff);
+    if (send == false)
+	return(rtnval);
+    else if (size > sizeof(buff) || (strlen(motor_call->message) + size) > MAX_MSG_SIZE)
+	errlogMessage("PIC862_build_trans(): buffer overflow.\n");
+    else
+    {
+	strcat(motor_call->message, buff);
+    }
+    return(rtnval);
+}
---
+++ motorApp/PiSrc/drvPIC862.h
@@ -0,0 +1,163 @@
+/* File: drvPIC862.h             */
+
+
+/* Device Driver Support definitions for motor */
+/*
+ *      Original Author: Ron Sluiter
+ *      Current Author: Mohan Ramanathan
+ *      Date: 09/04/2006
+ *
+ * Modification Log:
+ * -----------------
+ * .00  09/05/2006  mr  copied from drvPIC848.h
+ */
+
+#ifndef	INCdrvPIC862h
+#define	INCdrvPIC862h 1
+
+#include "motordrvCom.h"
+#include "asynDriver.h"
+#include "asynDriver.h"
+#include "asynOctetSyncIO.h"
+
+#define COMM_TIMEOUT 2		/* Timeout in seconds. */
+
+struct PIC862controller
+{
+    asynUser *pasynUser;	/* asynUser structure */
+    int asyn_address;		/* Use for GPIB or other address with asyn */
+    CommStatus status;		/* Controller communication status. */
+    char asyn_port[80];		/* asyn port name */
+};
+
+
+/*  LM629 Status bits */
+typedef union
+{
+    epicsUInt16 All;
+    struct
+    {
+#ifdef MSB_First
+	unsigned int motor_off		:1; /* 7 - Motor loop OFF. */
+	unsigned int breakpt		:1; /* 6 - Breakpoint reached flag. */
+	unsigned int max_pos_err	:1; /* 5 - Maximum position error is exceeded. */
+	unsigned int max_pos_lmt	:1; /* 4 - Maximum position Limit exceeded. */
+	unsigned int index		:1; /* 3 - Index pulse received flag. */
+	unsigned int trty_done		:1; /* 2 - Trajectory Complete. */
+	unsigned int cmnd_err		:1; /* 1 - Command error flag. */
+	unsigned int busy		:1; /* 0 - Busy. */
+#else
+	unsigned int busy		:1; /* 0 - Busy. */
+	unsigned int cmnd_err		:1; /* 1 - Command error flag. */
+	unsigned int trty_done		:1; /* 2 - Trajectory Complete. */
+	unsigned int index		:1; /* 3 - Index pulse received flag. */
+	unsigned int max_pos_lmt	:1; /* 4 - Maximum position Limit exceeded. */
+	unsigned int max_pos_err	:1; /* 5 - Maximum position error is exceeded. */
+	unsigned int breakpt		:1; /* 6 - Breakpoint reached flag. */
+	unsigned int motor_off		:1; /* 7 - Motor loop OFF. */
+#endif
+    } Bits;                                
+} C862_Status_Reg1;
+
+/*  Internal operation flags */
+typedef union
+{
+    epicsUInt16 All;
+    struct
+    {
+#ifdef MSB_First
+	unsigned int brd_addr		:1; /* 7 - Board Addressed. */
+	unsigned int num_mode		:1; /* 6 - Number mode in Effect. */
+	unsigned int ldzero_dis		:1; /* 3 - Leading Zero Suppression disabled. */
+	unsigned int macro_act		:1; /* 4 - Macro Command called. */
+	unsigned int ldzero_act		:1; /* 3 - Leading Zero Suppression active. */
+	unsigned int cmnd_err		:1; /* 2 - Command error flag. */
+	unsigned int wait_prgs		:1; /* 1 - Wait in Progress. */
+	unsigned int echo_on		:1; /* 0 - Echo ON. */
+#else
+	unsigned int echo_on		:1; /* 0 - Echo ON. */
+	unsigned int wait_prgs		:1; /* 1 - Wait in Progress. */
+	unsigned int cmnd_err		:1; /* 2 - Command error flag. */
+	unsigned int ldzero_act		:1; /* 3 - Leading Zero Suppression active. */
+	unsigned int macro_act		:1; /* 4 - Macro Command called. */
+	unsigned int ldzero_dis		:1; /* 3 - Leading Zero Suppression disabled. */
+	unsigned int num_mode		:1; /* 6 - Number mode in Effect. */
+	unsigned int brd_addr		:1; /* 7 - Board Addressed. */
+#endif
+    } Bits;                                
+} C862_Status_Reg2;
+
+/* Motor loop flags */
+typedef union
+{
+    epicsUInt16 All;
+    struct
+    {
+#ifdef MSB_First
+	unsigned int int_comm		:1; /* 7 - Command error flag. */
+	unsigned int move_err_ex	:1; /* 6 - Move Error Excess flag. */
+	unsigned int na34	        :2;
+	unsigned int move_err		:1; /* 3 - Move Error */
+	unsigned int mvdir_pol		:1; /* 2 - Move driection polarity. */
+	unsigned int na30	        :2;
+#else
+	unsigned int na30	        :2;
+	unsigned int mvdir_pol		:1; /* 2 - Move driection polarity. */
+	unsigned int move_err		:1; /* 3 - Move Error */
+	unsigned int na34	        :2;
+	unsigned int move_err_ex	:1; /* 6 - Move Error Excess flag. */
+	unsigned int int_comm		:1; /* 7 - Command error flag. */
+#endif
+    } Bits;                                
+} C862_Status_Reg3;
+
+/* Signal lines status */
+typedef union
+{
+    epicsUInt16 All;
+    struct
+    {
+#ifdef MSB_First
+	unsigned int na44	        :4;
+	unsigned int brake_on		:1; /* 3 - Brake ON. */
+	unsigned int find_prgs		:1; /* 2 - Find Edge Operation in Progress. */
+	unsigned int lmt_high		:1; /* 1 - Limit Switch Active Stet HIGH */
+	unsigned int lmt_on		:1; /* 0 - Limit Swicth ON flag. */
+#else
+	unsigned int lmt_on		:1; /* 0 - Limit Swicth ON flag. */
+	unsigned int lmt_high		:1; /* 1 - Limit Switch Active Stet HIGH */
+	unsigned int find_prgs		:1; /* 2 - Find Edge Operation in Progress. */
+	unsigned int brake_on		:1; /* 3 - Brake ON. */
+	unsigned int na44	        :4;
+#endif
+    } Bits;                                
+} C862_Status_Reg4;
+
+/* Signal lines inputs */
+typedef union
+{
+    epicsUInt16 All;
+    struct
+    {
+#ifdef MSB_First
+	unsigned int na54	        :4;
+	unsigned int minus_ls		:1; /* 3 - Negative limit signal flag. */
+	unsigned int plus_ls		:1; /* 2 - Positive limit signal flag. */
+	unsigned int index		:1; /* 1 - Reference signal flag */
+	unsigned int na50	        :1;
+#else
+	unsigned int na50	        :1;
+	unsigned int index		:1; /* 1 - Reference signal flag */
+	unsigned int plus_ls		:1; /* 2 - Positive limit signal flag. */
+	unsigned int minus_ls		:1; /* 3 - Negative limit signal flag. */
+	unsigned int na54	        :4;
+#endif
+    } Bits;                                
+} C862_Status_Reg5;
+
+
+/* Function prototypes. */
+extern RTN_STATUS PIC862Setup(int, int);
+extern RTN_STATUS PIC862Config(int, const char *, int);
+
+#endif	/* INCdrvPIC862h */
