c65b7f44a234ffcfb71411daeb0425b33f73bef9
MarkRivers <rivers@cars.uchicago.edu>
2011-03-28 18:23:35 +0000

    Changed a few variable names to avoid abbreviations

--- motorApp/MotorSrc/drvMotorAsyn.c
+++ motorApp/MotorSrc/drvMotorAsyn.c
@@ -84,8 +84,8 @@ typedef enum {
     motorPgain = motorAxisPGain,
     motorIgain = motorAxisIGain,
     motorDgain = motorAxisDGain,
-    motorHighLim = motorAxisHighLimit,
-    motorLowLim = motorAxisLowLimit,
+    motorHighLimit = motorAxisHighLimit,
+    motorLowLimit = motorAxisLowLimit,
     motorSetClosedLoop = motorAxisClosedLoop,
     motorEncoderPosition = motorAxisEncoderPosn,
     motorDeferMoves = motorAxisDeferMoves,
@@ -126,8 +126,8 @@ static motorCommandStruct motorCommands[] = {
     {motorPgain,                motorPgainString},
     {motorIgain,                motorIgainString},
     {motorDgain,                motorDgainString},
-    {motorHighLim,              motorHighLimString},
-    {motorLowLim,               motorLowLimString},
+    {motorHighLimit,            motorHighLimitString},
+    {motorLowLimit,             motorLowLimitString},
     {motorSetClosedLoop,        motorSetClosedLoopString},
     {motorDeferMoves,           motorDeferMovesString},
     {motorStatus,               motorStatusString},
@@ -739,8 +739,8 @@ static asynStatus writeFloat64(void *drvPvt, asynUser *pasynUser,
     case motorPgain:
     case motorIgain:
     case motorDgain:
-    case motorHighLim:
-    case motorLowLim:
+    case motorHighLimit:
+    case motorLowLimit:
     default:
 	status = (*pPvt->drvset->setDouble)(pAxis->axis, command, value);
 	break;
@@ -778,14 +778,14 @@ static asynStatus readGenericPointer(void *drvPvt, asynUser *pasynUser,
 	(*pPvt->drvset->getDouble)(pAxis->axis, motorPosition,
 				   &(value->position));
 	(*pPvt->drvset->getDouble)(pAxis->axis, motorEncoderPosition,
-				   &(value->encoder_posn));
+				   &(value->encoderPosition));
 	(*pPvt->drvset->getDouble)(pAxis->axis, motorVelocity,
 				   &(value->velocity));
     }
 						     
     asynPrint(pasynUser, ASYN_TRACEIO_DRIVER,
               "drvMotorAsyn::readMotorStatus, [%08x,%f,%f,%f]\n",
-	      value->status, value->position, value->encoder_posn,
+	      value->status, value->position, value->encoderPosition,
 	      value->velocity);
     return(asynSuccess);
 }
@@ -857,7 +857,7 @@ static void intCallback(void *axisPvt, unsigned int nChanged,
 	}
 	if (changed[i] == motorEncoderPosition) {
 	    (*pPvt->drvset->getDouble)(pAxis->axis, changed[i], 
-				       &(pAxis->status.encoder_posn));
+				       &(pAxis->status.encoderPosition));
 	}
 	/*	if (changed[i] == motorVelocity) {
 	    (*pPvt->drvset->getDouble)(pAxis->axis, changed[i], 
