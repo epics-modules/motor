0ec77b79e6f0aeec48384a99874c718b54156122
Ron Sluiter <rsluiter@users.noreply.github.com>
2011-08-25 16:14:24 +0000

    Replaced '__GNUG__' with '(TOOL_FAMILY == gnu)' in debug message macro.

--- motorApp/MotorSrc/motorRecord.cc
+++ motorApp/MotorSrc/motorRecord.cc
@@ -175,7 +175,7 @@ HeadURL:        $URL$
 
 /*----------------debugging-----------------*/
 
-#ifdef __GNUG__
+#if (TOOL_FAMILY == gnu)
     #ifdef  DEBUG
         #define Debug(l, f, args...) {if (l <= motorRecordDebug) printf(f, ## args);}
     #else
