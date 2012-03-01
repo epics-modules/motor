9fef2ffe9406246fa0d487635c5f336138b06f82
MarkRivers <rivers@cars.uchicago.edu>
2012-03-01 20:21:33 +0000

    Fix nested comments

--- motorApp/NewportSrc/XPS_C8_errors.h
+++ motorApp/NewportSrc/XPS_C8_errors.h
@@ -1,6 +1,6 @@
 /*///////////////////////////////////////////////////////////////////
-// Created header file XPS_C8_errors.h for XPS function errors
-//*/
+ * Created header file XPS_C8_errors.h for XPS function errors
+ */
 
 /* TCL interpretor error */
 #define ERR_TCL_INTERPRETOR_ERROR 1
