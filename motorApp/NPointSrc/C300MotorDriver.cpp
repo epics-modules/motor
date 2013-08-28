1af1b84179c237ccc76152556813a2983e9e34fb
MarkRivers <rivers@cars.uchicago.edu>
2013-08-28 16:14:12 +0000

    Changed abs to fabs to avoid compiler error on Windows; other changes to avoid compiler warnings on Linux

--- motorApp/NPointSrc/C300MotorDriver.cpp
+++ motorApp/NPointSrc/C300MotorDriver.cpp
@@ -18,6 +18,7 @@ KNOWN PROBLEMS:
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include <iocsh.h>
 #include <epicsThread.h>
@@ -54,7 +55,6 @@ C300Controller::C300Controller(const char *portName, const char *C300PortName, i
 {
   int axis;
   asynStatus status;
-  C300Axis *pAxis;
   static const char *functionName = "C300Controller";
 
   /* Connect to C300 controller */
@@ -76,7 +76,7 @@ C300Controller::C300Controller(const char *portName, const char *C300PortName, i
 
   // Create the axis objects
   for (axis=0; axis<numAxes; axis++) {
-    pAxis = new C300Axis(this, axis);
+    new C300Axis(this, axis);
   }
 
   startPoller(movingPollPeriod, idlePollPeriod, 2);
@@ -94,9 +94,7 @@ C300Controller::C300Controller(const char *portName, const char *C300PortName, i
 extern "C" int C300CreateController(const char *portName, const char *C300PortName, int numAxes, 
                                    int movingPollPeriod, int idlePollPeriod)
 {
-  C300Controller *pC300Controller
-    = new C300Controller(portName, C300PortName, numAxes, movingPollPeriod/1000., idlePollPeriod/1000.);
-  pC300Controller = NULL;
+  new C300Controller(portName, C300PortName, numAxes, movingPollPeriod/1000., idlePollPeriod/1000.);
   return(asynSuccess);
 }
 
@@ -281,7 +279,6 @@ asynStatus C300Axis::move(double position, int relative, double minVelocity, dou
 asynStatus C300Axis::poll(bool *moving)
 { 
   int done;
-  int num;
   asynStatus comStatus;
 
   // Read the current encoder position
@@ -293,9 +290,9 @@ asynStatus C300Axis::poll(bool *moving)
   //printf("\naxisName_ = %s\n", axisName_);
   //printf("data string = %s\n", pC_->inString_);
 
-  num = sscanf(&pC_->inString_[C300_DATA_OFFSET], "%lG", &posMonCorrectedVal_); 
+  sscanf(&pC_->inString_[C300_DATA_OFFSET], "%lG", &posMonCorrectedVal_); 
   //printf("data string = %s\n", pC_->inString_);
-  num = sscanf(&pC_->inString_[C300_CORR_OFFSET], "%lG", &analogInScaledVal_);
+  sscanf(&pC_->inString_[C300_CORR_OFFSET], "%lG", &analogInScaledVal_);
   //printf("data string = %s\n", pC_->inString_);
   
   //printf("posMonCorrectedVal_ = %lG\n", posMonCorrectedVal_); 
@@ -320,7 +317,7 @@ asynStatus C300Axis::poll(bool *moving)
   // Read error status
 
   // Simulate dmov functionality here
-  done = (abs(theoryPosition_ - encoderPosition_) > C300Tolerance) ? 0 : 1;
+  done = (fabs(theoryPosition_ - encoderPosition_) > C300Tolerance) ? 0 : 1;
   setIntegerParam(pC_->motorStatusDone_, done);
   *moving = done ? false:true;
 
