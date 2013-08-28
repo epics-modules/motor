f088b3319da8a9a47489bfef71a115891c365143
MarkRivers <rivers@cars.uchicago.edu>
2013-08-28 16:16:03 +0000

    Moved epicsExport.h to compile on Windows dynamically

--- motorApp/PIGCS2Src/PIasynAxis.cpp
+++ motorApp/PIGCS2Src/PIasynAxis.cpp
@@ -30,12 +30,13 @@ Based on drvMotorSim.c, Mark Rivers, December 13, 2009
 #include <epicsMutex.h>
 #include <ellLib.h>
 #include <iocsh.h>
-#include <epicsExport.h>
 #include <motor_interface.h>
 
 #include "PIasynAxis.h"
 #include "PIasynController.h"
 #include "PIGCSController.h"
+
+#include <epicsExport.h>
 
 //#undef asynPrint
 //#define asynPrint(user,reason,format...) 0
