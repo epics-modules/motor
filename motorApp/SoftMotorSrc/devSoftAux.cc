fc2b02582e07012d547082437a8fa4657e1a26fc
Ron Sluiter <rsluiter@users.noreply.github.com>
2015-03-13 19:11:14 +0000

    Reformat from tabs to spaces; no code changes.

--- motorApp/SoftMotorSrc/devSoftAux.cc
+++ motorApp/SoftMotorSrc/devSoftAux.cc
@@ -1,6 +1,6 @@
 /*
-FILENAME...	devSoftAux.cc
-USAGE...	Motor record device level support for Soft channel.
+FILENAME...     devSoftAux.cc
+USAGE...        Motor record device level support for Soft channel.
 
 Version:        $Revision$
 Modified By:    $Author$
@@ -28,7 +28,7 @@ HeadURL:        $URL$
  * -----------------
  * .01 06-16-03 rls Convert to R3.14.x.
  * .02 12-14-04 rls With EPICS R3.14.7 changes to epicsThread.h, need explicit
- *		    #include <stdlib.h>
+ *                  #include <stdlib.h>
  * .03 2006-04-10 pnd Convert to linked lists to remove arbitrary maximum
  */
 
@@ -52,7 +52,7 @@ in the same file; each defines (redefines) the DBR's.
 #include "motor.h"
 #include "devSoft.h"
 
-#define	STATIC	static
+#define STATIC  static
 
 STATIC void soft_dinp(struct event_handler_args);
 STATIC void soft_rdbl(struct event_handler_args);
@@ -84,38 +84,38 @@ long soft_init(void *after)
 
     if (before_after == 0)
     {
-	epicsThreadId dbCaTask_tid;
-	unsigned int soft_motor_priority;
-	int retry = 0;
+        epicsThreadId dbCaTask_tid;
+        unsigned int soft_motor_priority;
+        int retry = 0;
 
         soft_motor_sem = epicsEventCreate(epicsEventEmpty);
-	ellInit(&soft_motor_list);
+        ellInit(&soft_motor_list);
 
-	/* 
-	 * Fix for DMOV processing before the last DRBV update; i.e., lower
-	 * the priority of the "soft_motor" task below the priority of the
-	 * "dbCaLink" task.
-	 */
-	while((dbCaTask_tid = epicsThreadGetId("dbCaLink")) == 0 && retry < 10)
-	{
-	    epicsThreadSleep(0.1);
-	    retry++;
-	}
+        /* 
+         * Fix for DMOV processing before the last DRBV update; i.e., lower
+         * the priority of the "soft_motor" task below the priority of the
+         * "dbCaLink" task.
+         */
+        while((dbCaTask_tid = epicsThreadGetId("dbCaLink")) == 0 && retry < 10)
+        {
+            epicsThreadSleep(0.1);
+            retry++;
+        }
 
-	if (dbCaTask_tid == 0)
-	{
-	    errMessage(0, "cannot find dbCaLink task.");
-	    return(ERROR);
-	}
-	soft_motor_priority = epicsThreadGetPriority(dbCaTask_tid);
-	soft_motor_priority -= 1;
+        if (dbCaTask_tid == 0)
+        {
+            errMessage(0, "cannot find dbCaLink task.");
+            return(ERROR);
+        }
+        soft_motor_priority = epicsThreadGetPriority(dbCaTask_tid);
+        soft_motor_priority -= 1;
 
-	soft_motor_id = epicsThreadCreate((char *) "soft_motor", soft_motor_priority, 
+        soft_motor_id = epicsThreadCreate((char *) "soft_motor", soft_motor_priority, 
                                           epicsThreadGetStackSize(epicsThreadStackBig),
-					  (EPICSTHREADFUNC) soft_motor_task, NULL);
+                                          (EPICSTHREADFUNC) soft_motor_task, NULL);
     }
     else
-	epicsEventSignal(soft_motor_sem);	/* Start soft_motor_task(). */
+        epicsEventSignal(soft_motor_sem);       /* Start soft_motor_task(). */
     return(OK);
 }
 
@@ -129,12 +129,11 @@ long soft_init_record(void *arg)
     int status = 0;
 
     list_entry = (struct motor_node *) malloc(sizeof(struct motor_node));
-    if (!list_entry) {
-	return(ERROR);
-    }
+    if (!list_entry)
+        return(ERROR);
 
     list_entry->pmr = mr;
-    ellAdd(&soft_motor_list, (ELLNODE*)list_entry);
+    ellAdd(&soft_motor_list, (ELLNODE*) list_entry);
 
     /* Allocate space for private field. */
     mr->dpvt = (struct soft_private *) malloc(sizeof(struct soft_private));
@@ -143,8 +142,7 @@ long soft_init_record(void *arg)
     ptr->initialized = false;
 
     cbptr = &ptr->callback;
-    callbackSetCallback((void (*)(struct callbackPvt *)) soft_motor_callback,
-			cbptr);
+    callbackSetCallback((void (*)(struct callbackPvt *)) soft_motor_callback, cbptr);
     callbackSetPriority(priorityMedium, cbptr);
     callbackSetUser(mr, cbptr);
     return ((long) status);
@@ -158,44 +156,41 @@ STATIC EPICSTHREADFUNC soft_motor_task(void *parm)
     chid dinp, rdbl, rinp;
     epicsEventId wait_forever;
 
-    epicsEventWait(soft_motor_sem);	/* Wait for dbLockInitRecords() to execute. */
-    SEVCHK(ca_context_create(ca_enable_preemptive_callback),
-	   "soft_motor_task: ca_context_create() error");
+    epicsEventWait(soft_motor_sem);     /* Wait for dbLockInitRecords() to execute. */
+    SEVCHK(ca_context_create(ca_enable_preemptive_callback), "soft_motor_task: ca_context_create() error");
 
-    while ((node = (struct motor_node *)ellGet(&soft_motor_list)))
+    while ((node = (struct motor_node *) ellGet(&soft_motor_list)))
     {
-	struct soft_private *ptr;
+        struct soft_private *ptr;
 
-	mr = node->pmr;
-	free(node);
+        mr = node->pmr;
+        free(node);
 
-	ptr = (struct soft_private *) mr->dpvt;
-	if (mr->dinp.value.constantStr == NULL)
-	{
-	    ptr->default_done_behavior = true;
-	}
-	else
-	{
-	    ptr->default_done_behavior = false;
-	    SEVCHK(ca_search(mr->dinp.value.pv_link.pvname, &dinp),
-		   "ca_search() failure");
-	    SEVCHK(ca_add_event(DBR_SHORT, dinp, soft_dinp, mr, NULL),"ca_add_event() failure");
-	    SEVCHK(ca_pend_io((float) 5.0), "DINP link failure");
-	}
+        ptr = (struct soft_private *) mr->dpvt;
+        if (mr->dinp.value.constantStr == NULL)
+            ptr->default_done_behavior = true;
+        else
+        {
+            ptr->default_done_behavior = false;
+            SEVCHK(ca_search(mr->dinp.value.pv_link.pvname, &dinp),
+                   "ca_search() failure");
+            SEVCHK(ca_add_event(DBR_SHORT, dinp, soft_dinp, mr, NULL),"ca_add_event() failure");
+            SEVCHK(ca_pend_io((float) 5.0), "DINP link failure");
+        }
     
-	if (mr->urip != 0)
-	{
-	    SEVCHK(ca_search(mr->rdbl.value.pv_link.pvname, &rdbl),"ca_search() failure");
-	    SEVCHK(ca_add_event(DBR_DOUBLE, rdbl, soft_rdbl, mr, NULL),"ca_add_event() failure");
-	    SEVCHK(ca_pend_io((float) 5.0), "RDBL link failure");
-	}
+        if (mr->urip != 0)
+        {
+            SEVCHK(ca_search(mr->rdbl.value.pv_link.pvname, &rdbl),"ca_search() failure");
+            SEVCHK(ca_add_event(DBR_DOUBLE, rdbl, soft_rdbl, mr, NULL),"ca_add_event() failure");
+            SEVCHK(ca_pend_io((float) 5.0), "RDBL link failure");
+        }
 
-	if (mr->rinp.value.constantStr != NULL)
-	{
-	    SEVCHK(ca_search(mr->rinp.value.pv_link.pvname, &rinp),"ca_search() failure");
-	    SEVCHK(ca_add_event(DBR_LONG, rinp, soft_rinp, mr, NULL),"ca_add_event() failure");
-	    SEVCHK(ca_pend_io((float) 5.0), "RINP link failure");
-	}
+        if (mr->rinp.value.constantStr != NULL)
+        {
+            SEVCHK(ca_search(mr->rinp.value.pv_link.pvname, &rinp),"ca_search() failure");
+            SEVCHK(ca_add_event(DBR_LONG, rinp, soft_rinp, mr, NULL),"ca_add_event() failure");
+            SEVCHK(ca_pend_io((float) 5.0), "RINP link failure");
+        }
     }
 
     ellFree(&soft_motor_list);
@@ -203,9 +198,9 @@ STATIC EPICSTHREADFUNC soft_motor_task(void *parm)
        thread, so as not to show up in the thread list as "SUSPENDED", which
        is usually a sign of a fault. */
     wait_forever = epicsEventCreate(epicsEventEmpty);
-    if (wait_forever) {
-	epicsEventMustWait(wait_forever);
-    }
+    if (wait_forever)
+        epicsEventMustWait(wait_forever);
+
     return(NULL);
 }
 
