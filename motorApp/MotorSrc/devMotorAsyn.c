71e417b4dbd6da543ff8a87649958d7c107069d6
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-09-18 18:04:05 +0200

    define VERSION_INT when using older EPICS bases
    Newer EPICS base versions define VERSION_INT, but older don't.
    Add a conditional #define to compile against those older bases, like 3.14

--- motorApp/MotorSrc/devMotorAsyn.c
+++ motorApp/MotorSrc/devMotorAsyn.c
@@ -47,6 +47,10 @@
 
 #include "epicsVersion.h"
 
+#ifndef VERSION_INT
+#  define VERSION_INT(V,R,M,P) ( ((V)<<24) | ((R)<<16) | ((M)<<8) | (P))
+#endif
+
 #define VERSION_INT_3_16 VERSION_INT(3,16,0,0)
 #if EPICS_VERSION_INT < VERSION_INT_3_16
 #define RECSUPFUN_CAST (RECSUPFUN)
