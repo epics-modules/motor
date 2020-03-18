c171e760964f458c3da99d8f6eacbcd60b519f28
Kevin Peterson <kmpeters@anl.gov>
2020-03-18 10:57:56 -0500

    Added .ci-local containing a post-prepare.sh, to allow the travis environment to be modified before building.

---
+++ .ci-local/travis/post-prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Set VV in .travis.yml to make scripts verbose
+[ "$VV" ] && set -x
+
+CACHEDIR=${CACHEDIR:-${HOME}/.cache}
+
+# source functions
+. $SCRIPTDIR/utils.sh
+
+# Add SUPPORT to RELEASE.local
+update_release_local SUPPORT ${CACHEDIR}
+
+echo -e "${ANSI_BLUE}Updated contents of RELEASE.local${ANSI_RESET}"
+cat ${CACHEDIR}/RELEASE.local
