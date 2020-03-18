f5c8c39d0cfccf45390727de9e28a5d05b4cbdf4
Kevin Peterson <kmpeters@anl.gov>
2020-03-18 11:29:32 -0500

    Modified post-prepare.sh to copy the RELEASE.local file after modifying it.

--- .ci-local/travis/post-prepare.sh
+++ .ci-local/travis/post-prepare.sh
@@ -12,8 +12,10 @@ pwd
 # source functions
 . ./.ci/travis/utils.sh
 
-# Add SUPPORT to RELEASE.local
+# Add SUPPORT to RELEASE.local in the cache directory
 update_release_local SUPPORT ${CACHEDIR}
+# Copy the RELEASE.local from the cache directory to motor's configure directory
+[ -e ./configure ] && cp -f ${CACHEDIR}/RELEASE.local ./configure/RELEASE.local
 
 echo -e "${ANSI_BLUE}Updated contents of RELEASE.local${ANSI_RESET}"
 cat ${CACHEDIR}/RELEASE.local
