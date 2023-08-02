1dec3cb3fbf4268d60f3f0dc2c43a42c31ab0030
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2023-08-02 16:41:31 +0100

    Adding master to acs (#4)

--- uploadtosource.sh
+++ uploadtosource.sh
@@ -1,21 +1,21 @@
 rm -rf upload_dir
 mkdir upload_dir
 cd upload_dir
+git clone git@github.com:CentralLaserFacility/source-downloads.git -b master
 git clone git@github.com:CentralLaserFacility/motor.git -b development_clf --recurse-submodules motor-R7-2-2
 
 tar -czvf motor-R7-2-2.tar.gz motor-R7-2-2/
 
 rm -rf motor-R7-2-2
 
-git clone https://github.com/CentralLaserFacility/source-downloads -b master
 
-mv source-downloads/EPICS/test source-downloads/EPICS/test_"`date +"%d-%m-%Y"`"
+mv source-downloads/EPICS/motor-R7-2-2.tar.gz source-downloads/EPICS/old_"`date +"%T%Z%d-%m-%Y"`"
 mv motor-R7-2-2.tar.gz source-downloads/EPICS/
 
 cd source-downloads/
 
 git add --all
-git commit -m "Updated motor-R7-2-2 on `date +"%d-%m-%Y"`"
+git commit -m "Updated motor-R7-2-2 on `date +"%T%Z %d-%m-%Y"`"
 git push
 
 rm -rf upload_dir
